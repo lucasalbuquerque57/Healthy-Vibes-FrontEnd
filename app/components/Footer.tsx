30c5b944ff500b7b26812bab9e4ca613b0f55d6e
Lucas Albuquerque <75431427+spartan117navy@users.noreply.github.com>
2023-09-20 16:36:35 -0300

    Footer da Berato

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,15 +1,77 @@
-                                                         
+import { Link } from "@remix-run/react";
+                                                      
 
 
 export function Footer() {
     
     return (
         <footer>
-            <div>
-                <h3>Redes Sociais</h3>
-                <img src="" alt="logo" />
+        <div>
+            <h3 className="footer-logo">Logo</h3>
+            <p>Redes Sociais</p>
+            <div className="footer-socials">
+                <Link to="#" className="footer-link" id="youtube">
+                    <i className="fa-brands fa-youtube"></i>
+                </Link>
+
+                <Link to="#" className="footer-link" id="whatsapp">
+                    <i className="fa-brands fa-whatsapp"></i>
+                </Link>
+
+                <Link to="#" className="footer-link" id="facebook">
+                    <i className="fa-brands fa-facebook-f"></i>
+                </Link>
+                
+                <Link to="#" className="footer-link" id="instagram">
+                    <i className="fa-brands fa-instagram"></i>
+                </Link>
             </div>
-        </footer>
+        </div>
+    <div>
+        
+    
+        <ul className="footer-list">
+              <li>
+                  <h3>Deixe-nos ajudar você</h3>
+              </li>
+              <li>
+                  <Link to="#" className="footer-link">Receita</Link>
+              </li>
+              <li>
+                  <Link to="#" className="footer-link">Dietas</Link>
+              </li>
+              <li>
+                  <Link to="#" className="footer-link">Calculadora</Link>
+              </li>
+              <li>
+                  <Link to="#" className="footer-link">Exercícios</Link>
+              </li>
+              <li>
+                  <Link to="#" className="footer-link">Controle de Água</Link>
+              </li>
+        </ul>
+    </div>
+    <div>
+        <ul className="footer-list">
+            <li>
+                <h3>Bem-vindo</h3>
+            </li>
+            <li>
+                <Link to="#" className="footer-link">Sobre Nos</Link>
+            </li>
+            <li>
+                <Link to="#" className="footer-link">Contato</Link>
+            </li>
+            <li>
+                <Link to="#" className="footer-link">Termos de Serviço</Link>
+            </li>
+            <li>
+                <Link to="#" className="footer-link">Política de Privacidade</Link>
+            </li>
+        </ul>
+    </div>
+            
+    </footer>
     )
 
 }
--- app/root.tsx
+++ app/root.tsx
@@ -25,6 +25,8 @@ export const links: LinksFunction = () => {
   return [
     { rel: "stylesheet", href: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css", integrity: "sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ", crossOrigin: "anonymous" },
     { rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" },
+    { rel: "stylesheet", href: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css", integrity: "sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==", crossOrigin: "anonymous"},
+
     { rel: "shortcut icon", href: "/IconeLogo.png", type: "image/x-icon" },
     { rel: "stylesheet", href: main }
   ];
