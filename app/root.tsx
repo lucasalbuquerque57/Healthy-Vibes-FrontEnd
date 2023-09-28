a1daf92f4ce686398dc79ffa78b424cd435b13d4
Lucas Albuquerque <75431427+spartan117navy@users.noreply.github.com>
2023-09-28 12:04:17 -0300

    Fonte padrao do projeto alterada

--- app/root.tsx
+++ app/root.tsx
@@ -24,6 +24,7 @@ export const links: LinksFunction = () => {
     { rel: "stylesheet", href: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css", integrity: "sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN", crossOrigin: "anonymous" },
     { rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" },
     { rel: "stylesheet", href: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css", integrity: "sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==", crossOrigin: "anonymous" },
+    { rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Inter&display=swap", crossOrigin: "anonymous" },
 
     { rel: "shortcut icon", href: "/IconeLogo.png", type: "image/x-icon" },
     { rel: "stylesheet", href: main }
