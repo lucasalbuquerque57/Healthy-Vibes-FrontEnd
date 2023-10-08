220f3c77aed5563d2ba5ecd0b591e209781e90af
DaltonGuima <dalton.guimaraes500@gmail.com>
2023-10-07 22:17:09 -0300

    Terminei a barra do Perfil

--- app/routes/Profile.dietas/route.tsx
+++ app/routes/Profile.dietas/route.tsx
@@ -1,3 +1,4 @@
+import { Link } from "@remix-run/react";
 import { CardDieta } from "./Card_Dieta";
 
 export default function dietasProfile() {
@@ -14,16 +15,12 @@ export default function dietasProfile() {
                     <CardDieta
                         title="Dieta Sla"
                     />
-                    <CardDieta
-                        title="Dieta Sla"
-                    />
-                    <CardDieta
-                        title="Dieta Sla"
-                    />
-                    <CardDieta
-                        title="Dieta Sla"
-                    />
                 </section>
+            </div>
+            <div className="container-fluid mb-5">
+                <Link to="/CalculadoraNutricional">
+                    <button className="float-end stylebutton me-4 p-0 px-4 py-2" type="button">Adicionar Dieta</button>
+                </Link>
             </div>
         </main>
     )
