6dbe8b6fa3b0043bd71b587f11c02c717c4d5645
Lucas Albuquerque <75431427+spartan117navy@users.noreply.github.com>
2023-10-10 12:35:34 -0300

    clone da versao no netlify

--- remix.config.js
+++ remix.config.js
@@ -1,10 +1,18 @@
+const baseConfig =
+  process.env.NODE_ENV === "production"
+    ? // when running the Netify CLI or building on Netlify, we want to use
+      {
+        server: "./server.js",
+        serverBuildPath: ".netlify/functions-internal/server.js",
+      }
+    : // otherwise support running remix dev, i.e. no custom server
+      undefined;
+
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
+  ...baseConfig,
   ignoredRouteFiles: ["**/.*"],
-  // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // serverBuildPath: "build/index.js",
-  // publicPath: "/build/",
+  // See https://remix.run/docs/en/main/file-conventions/route-files-v2
   serverModuleFormat: "cjs",
   future: {
     v2_dev: true,
@@ -18,4 +26,3 @@ module.exports = {
     /^axios.*/,
 ],
 };
-
