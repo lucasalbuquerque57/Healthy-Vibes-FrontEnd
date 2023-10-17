d0120bac1c3f096cb8752db8f3234ec42e0dd9cc
Giovanna Berato <giovanna.berato@gmail.com>
2023-10-17 15:46:23 -0300

    cards_exercicios

--- app/routes/Exercicios.tsx
+++ app/routes/Exercicios.tsx
@@ -117,6 +117,14 @@ export default function Exercicios() {
                         link="/"
                     />
 
+                    <ExercicioBase
+                        imgSrc="abraco.png"
+                        altImg="Um pão e copo de água"
+                        cardTitle="Recursos adicionais"
+                        textoCard="Monitore seu IMC, consumo de água e calorias"
+                        link="/"
+                    />
+
 
 
 
